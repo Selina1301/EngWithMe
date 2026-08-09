912faa100b24325116059e81109d9749fd4750e5
Tung Duong <kingcute5x@gmail.com>
2026-08-09 10:10:58 +0700

    UI polishing and rename Arena to Game

--- fe/js/core.js
+++ fe/js/core.js
@@ -32,7 +32,7 @@ function setActiveNav() {
     { href: "quiz.html", label: "Exam" },
     { href: "rank.html", label: "Rank" },
     { href: "blog.html", label: "Blog" },
-    { href: "game.html", label: "Arena" },
+    { href: "game.html", label: "Game" },
     { href: "pricing.html", label: "Premium" }
   ];
 
